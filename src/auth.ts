a63b65677b8786d55523b7790a66e427b9777f6b
Jonel Hatwell <jonel.hatwell@gmail.com>
2024-09-24 21:51:06 +0800

    add drizzle adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
-
+import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import authConfig from "@/auth.config"
+import { db } from "./drizzle"
  
 export const { 
   auth, 
@@ -12,4 +13,5 @@ export const {
   session: {
     strategy: 'jwt',
   },
+  adapter: DrizzleAdapter(db),
 })
